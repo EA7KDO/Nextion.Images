e486b9f94471568662e51d4e3591498d18cf560e
KF6S <43057968+KF6S@users.noreply.github.com>
2019-03-22 16:01:19 -0700

    Add files via upload

---
+++ getmodem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#########################################################
+# Get Modem Hat from Pi-Star                            #
+# KF6S                                      03-22-2019  #
+#########################################################
+# Use Pi-Star password from command $1
+password=$1
+if [ -z $1 ]
+  then
+     password="raspberry" 
+fi
+echo $password
+#Put Pi-Star file system in RW mode
+rpi-rw
+#Read MMDVM Hat version form Pi-Star web server
+sudo wget -q -O- --user pi-star --password $password 127.0.0.1/admin/configure.php | grep -A 50 'What kind of radio or modem hardware do you have?' | grep '<option selected="selected" value="'| awk '{print substr($3,8,match($3,">")-9)}'
