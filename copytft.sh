45530393d22f032ac600d671fd6b234a9dce2b2c
KF6S <43057968+KF6S@users.noreply.github.com>
2019-03-26 21:44:06 -0700

    Update copytft.sh

--- copytft.sh
+++ copytft.sh
@@ -8,7 +8,7 @@
 model=$1
 tft='.tft' gz='.gz'
 #Put Pi-Star file system in RW mode
-rpi-rw
+#sudo mount -o remount,rw /
 #Check to make sure that NO TFT file exists at the destination
 rm /usr/local/etc/$model$tft
 cp /tmp/Nextion.Images/$model$tft /usr/local/etc/$model$tft
