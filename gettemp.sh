856d8e8db293de3b5e74e13a24e9d03a8e01e499
KF6S <43057968+KF6S@users.noreply.github.com>
2019-04-02 22:58:47 -0700

    Update gettemp.sh

--- gettemp.sh
+++ gettemp.sh
@@ -4,6 +4,7 @@
 #Read Temp form Pi-Star web server
 C=`wget -q -O- --user pi-star --password raspberry 127.0.0.1/admin/ | grep -A 1 '&deg;C' | awk '{print substr($3,7,5)}' | sed 's/&//' | sed 's/d//'`
 F=`wget -q -O- --user pi-star --password raspberry 127.0.0.1/admin/ | grep -A 1 '&deg;C' | awk '{print substr($5,1,5)}' | sed 's/&//' | sed 's/d//'`
-echo "$F" "°F" "/" "$C" "°C"
+echo "$F" "   " "$C"
+
 
 
