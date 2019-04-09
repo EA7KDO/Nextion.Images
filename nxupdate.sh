9f2a2c89a9fb11535634578c57a171a6d3d5b969
KF6S <43057968+KF6S@users.noreply.github.com>
2019-04-09 09:43:44 -0700

    Update nxupdate.sh

--- nxupdate.sh
+++ nxupdate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #Run Pi-Star update before adding BC
 sudo pistar-update
+# set to R/W becuase pi-star update will try and set R/O
+sudo mount -o remount,rw /
 #Update files needed for new Nextion functions
 sudo apt-get install bc
+#set to R/O when finished
+sudo mount -o remount,ro /
