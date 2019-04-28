90e804c9ca13b16c65b8c40a38e42f484a540082
KF6S <43057968+KF6S@users.noreply.github.com>
2019-04-27 20:31:11 -0700

    Update gettemp.sh

--- gettemp.sh
+++ gettemp.sh
@@ -12,7 +12,7 @@ tempc=$(</sys/class/thermal/thermal_zone0/temp)
 tc=$(echo "scale=1;$tempc/1000" | bc)
 # formula tf=(9/5)*Tc+32
 tf=$(echo "scale=1;((9/5) * $tc) + 32" | bc)
-echo "$tf  $tc";
+echo "$tf   $tc";
 
 
 
