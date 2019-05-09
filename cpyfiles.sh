b2e3f9f9ec368c8690b1f786298a7a7d6dce5d1c
KF6S <43057968+KF6S@users.noreply.github.com>
2019-05-08 18:49:50 -0700

    Create cpyfiles.sh

---
+++ cpyfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#########################################################
+#  Nextion Support for Nextion screen. Used to copy     #
+#  files from Nextion.Images to there proper location   #
+#                                                       #
+#  KF6S                                     05-01-2019  #
+#########################################################
+
+sudo mount -o remount,rw /
+sudo mkdir /usr/local/etc/Nextion_Support
+sudo cp /tmp/Nextion.Images/{*.sh,*.fw} /usr/local/etc/Nextion_Support
+sudo chmod +x /usr/local/etc/Nextion_Support/*.sh
