d305d46780a1035c6362ace9554affc647fb668e
KF6S <43057968+KF6S@users.noreply.github.com>
2019-05-20 16:39:00 -0700

    Create chgdmrmaster.sh

---
+++ chgdmrmaster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+############################################################
+#  Change DMR Master in /etc/mmdvmhost, find old value and #
+#  substitute with new value from Profie 2 screen passed   #
+#  in $1.                                                  #
+#                                                          #
+#  KF6S                                        05-20-2019  #
+############################################################
+set -o errexit
+set -o pipefail
+# Use passed DMR Master if present or default to TGIF if missing
+if [ -z "$1" ]; then
+NewHost="Address=74.91.125.81"
+else
+NewHost="Address="$1
+fi
+
+# Get old DMR Host from /etc/mmdvmhost
+Host=$(sudo cat /etc/mmdvmhost | grep "DMR Network" -A 6 | grep "Address=")
+# Replace with host that was selected on the Profiles 2 Nextion screen
+sudo sed -i 's+'"$Host"'+'"$NewHost"'+' /etc/mmdvmhost;
