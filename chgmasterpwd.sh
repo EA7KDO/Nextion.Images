8441fb2f154ffc21d85f11fcfc90fe6d39475bbc
KF6S <43057968+KF6S@users.noreply.github.com>
2019-06-23 15:28:48 -0700

    Create chgmasterpwd.sh

---
+++ chgmasterpwd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+####################################################################
+# Change Master password in  /etc/mmdvmhost,                       #
+# find old value and substitute with new value from Profie wcreen  #
+# that as passed in $1.                                            #
+#                                                                  #
+# KF6S                                                 05-20-2019  #
+####################################################################
+set -o errexit
+set -o pipefail
+# Use passed DMR Master if present or default to TGIF if missing
+if [ -z "$1" ]; then
+NewHostPWD="Password=passw0rd"
+else
+NewHostPWD="Password="$1
+fi
+
+# Get old Host Password from /etc/mmdvmhost
+OldHostPWD=$(sudo cat /etc/mmdvmhost | grep "DMR Network" -A 10 | grep "Passwor$
+# Replace with host password that was selected on the Profile Nextion screen
+sudo sed -i 's+'"$OldHostPWD"'+'"$NewHostPWD"'+' /etc/mmdvmhost;
