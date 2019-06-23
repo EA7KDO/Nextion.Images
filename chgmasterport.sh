00b0df4a959200ebd89c841b34fa84f4ffb4f89e
KF6S <43057968+KF6S@users.noreply.github.com>
2019-06-23 15:26:50 -0700

    Create chgmasterport.sh

---
+++ chgmasterport.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+############################################################
+# Change Master Port in /etc/mmdvmhost, find old value and #
+# substitute with new value from Profie 2 screen passed    #
+# in $1.                                                   #
+#                                                          #
+#  KF6S                                        05-20-2019  #
+############################################################
+set -o errexit
+set -o pipefail
+# Use passed Master Port if present or default to 62031 if missing
+if [ -z "$1" ]; then
+NewHostPort="Port=62031"
+else
+NewHostPort="Port="$1
+fi
+
+# Get old Host Port from /etc/mmdvmhost
+Host=$(sudo cat /etc/mmdvmhost | grep "DMR Network" -A 10 | grep "Port=")
+# Replace with host port that was selected on the Profile Nextion screen
+sudo sed -i 's+'"$Host"'+'"$NewHostPort"'+' /etc/mmdvmhost;
