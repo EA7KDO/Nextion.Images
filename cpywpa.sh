5a448eede0afebcc2fe588ad0a56d18445fa228c
KF6S <43057968+KF6S@users.noreply.github.com>
2019-07-02 17:44:43 -0700

    Create cpywpa.sh

---
+++ cpywpa.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#########################################################
+#  Nextion Support for Nextion screen. Used to copy     #
+#  wpa_supplicant file to working location              #
+#                                                       #
+#  KF6S                                     05-01-2019  #
+#########################################################
+
+sudo mount -o remount,rw /
+sudo cp /usr/local/etc/Nextion_Support/wpa_supplicant.conf /boot/wpa_supplicant.conf
+status=$?
+
+if test $status -eq 0
+then
+        echo "Supplicant copy OK!"
+
+else
+        sudo mount -o remount,rw /
+        sudo cp /usr/local/etc/Nextion_Support/wpa_supplicant.conf /boot/wpa_supplicant$
+        status-$?
+        if test $status -eq 0
+        then
+        echo "Second attempt to copy Supplicant OK!
+        else
+        echo "Second attempt to copy Supplicant failed."
+        fi              
+fi
