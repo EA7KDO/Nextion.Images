4dc70d9cea7c8cea77c2ae47f294734376d1a10f
KF6S <43057968+KF6S@users.noreply.github.com>
2019-07-05 17:20:20 -0700

    Update cpywpa.sh

--- cpywpa.sh
+++ cpywpa.sh
@@ -18,12 +18,12 @@ then
 else
         sudo mount -o remount,rw /
         sleep 1s
-        sudo cp /usr/local/etc/Nextion_Support/wpa_supplicant.conf /boot/wpa_supplicant$
+        sudo cp /usr/local/etc/Nextion_Support/wpa_supplicant.conf /boot/wpa_supplicant
         status-$?
         if test $status -eq 0
         then
-        echo "Second attempt to copy Supplicant OK!
+        echo "Second attempt to copy Supplicant OK!"
         else
         echo "Second attempt to copy Supplicant failed!"
-        fi              
+        fi
 fi
