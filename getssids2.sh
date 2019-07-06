fe10d44e5db05d47bff7701fdf0e81bf6bc3948a
KF6S <43057968+KF6S@users.noreply.github.com>
2019-07-05 18:39:55 -0700

    Create getssids2.sh

---
+++ getssids2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+###############################################################
+# Support for Nextion Screen to format ESSID list for screen. #
+# This grabs the second part of the file so the returned data #
+# is < 90 characters. If the Nextion Driver trys to return    #
+# more than 90 characters it with crash.                      #
+#                                                             #
+# KF6S                                            07-05-2019  #
+###############################################################
+
+sudo mount -o remount,rw /
+sleep 1s
+input="/usr/local/etc/Nextion_Support/essid.txt"
+sudo rm /usr/local/etc/Nextion_Support/essid2.txt
+output="/usr/local/etc/Nextion_Support/essid2.txt"
+index=1
+while IFS= read -r line
+do
+  echo "#"$index "$line" "  " >> $output
+  index=$((index+1))
+done < "$input"
+cat /usr/local/etc/Nextion_Support/essid2.txt | tr -d '\n' > /usr/local/etc/Nextion_Support/essid3.txt
+cat /usr/local/etc/Nextion_Support/essid3.txt | awk '{ gsub ( "\"", "", $0); print substr( $0, 81, 160 ) }'
