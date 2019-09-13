ab3159d2e3717570462b6a1d6f422916db065bf9
KF6S <43057968+KF6S@users.noreply.github.com>
2019-09-12 18:39:51 -0700

    Create setmode.sh

---
+++ setmode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+############################################################
+#  Change Mode status in /etc/mmdvmhost                    #
+#  $1 1-6 Select Mode to change  $2 enable= 0 or 1         #
+#                                                          #
+#  KF6S                                        09-01-2019  #
+############################################################
+set -o errexit
+set -o pipefail
+# Check all six modes and set each one to either 0 or 1
+
+if [ -z "$1" ]; then
+        exit
+        else
+        if [ "$1" = 1 ]; then sudo sed -i '/\[D-Star\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+        fi
+
+        if [ "$1" = 2 ]; then sudo sed -i '/\[DMR\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+        fi
+
+        if [ "$1" = 3 ]; then sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+        fi
+
+        if [ "$1" = 4 ]; then sudo sed -i '/\[P25\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+        fi
+
+        if [ "$1" = 5 ]; then sudo sed -i '/\[NXDN\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+        fi
+
+        if [ "$1" = 6 ]; then sudo sed -i '/\[POCSAG\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+        fi
+fi;
