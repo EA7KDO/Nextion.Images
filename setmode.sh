c6a843babf311229679f87ee680714ab5eb144c1
KF6S <43057968+KF6S@users.noreply.github.com>
2019-09-14 10:37:53 -0700

    Update setmode.sh

--- setmode.sh
+++ setmode.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ############################################################
-#  Change Mode status in /etc/mmdvmhost                    #
+#  Change Mode in /etc/mmdvmhost                           #
 #  $1 1-6 Select Mode to change  $2 enable= 0 or 1         #
-#                                                          #
-#  KF6S                                        09-01-2019  #
+#  Also enable corresponding network                       #
+#  KF6S                                        09-14-2019  #
 ############################################################
 set -o errexit
 set -o pipefail
@@ -13,20 +13,26 @@ if [ -z "$1" ]; then
         exit
         else
         if [ "$1" = 1 ]; then sudo sed -i '/\[D-Star\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+                              sudo sed -i '/\[D-Star Network\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
         fi
 
         if [ "$1" = 2 ]; then sudo sed -i '/\[DMR\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+                              sudo sed -i '/\[DMR Network\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
         fi
 
         if [ "$1" = 3 ]; then sudo sed -i '/\[System Fusion\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+                              sudo sed -i '/\[System Fusion Network\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
         fi
 
         if [ "$1" = 4 ]; then sudo sed -i '/\[P25\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+                              sudo sed -i '/\[P25 Network\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
         fi
 
         if [ "$1" = 5 ]; then sudo sed -i '/\[NXDN\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+                              sudo sed -i '/\[NXDN Network\]/!b;n;n;n;n;n;n;cEnable='"$2"'' /etc/mmdvmhost
         fi
 
         if [ "$1" = 6 ]; then sudo sed -i '/\[POCSAG\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
+                              sudo sed -i '/\[POCSAG Network\]/!b;n;cEnable='"$2"'' /etc/mmdvmhost
         fi
 fi;
