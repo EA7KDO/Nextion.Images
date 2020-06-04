b6fdc592b1d08b979ded99e56f290e797c643477
KF6S <43057968+KF6S@users.noreply.github.com>
2020-06-04 14:04:01 -0700

    Update chkfile.sh

--- chkfile.sh
+++ chkfile.sh
@@ -2,16 +2,19 @@
 #########################################################
 #  Check if a file exists                               #
 #                                                       #
-#  KF6S 03-19-2019                                      #
+#  KF6S 06-04-2020                                      #
 #########################################################
 set -o errexit
 set -o pipefail
 #
 #echo $1
-if [ -e $1 ]
-then
-echo "FILE OK!"
+FILE="/usr/local/etc/Nextion_Support/$1"
+#echo $FILE
+
+if [ -f "$FILE" ]; then
+   echo "TRUE"
 else
-echo "FILE NOT FOUND!"
+   echo "FALSE"
 fi;
+
 
